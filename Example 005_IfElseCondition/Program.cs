f9593a9aef887de1380e3fcd57d100ae1174ca5b
Irina <tritty1@yandex.ru>
2023-07-25 12:32:53 +0300

    Example005

---
+++ Example 005_IfElseCondition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary");
+{
+    Console.WriteLine("Hurray, this is Mary! ");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
